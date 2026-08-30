b66fa25ee2da35c9cae873eaf3d4c8919f6ce343
jun-na <junpei500rr@gmail.com>
2026-08-30 09:17:41 +0900

    ファンクションキー実装

---
+++ Shell/FunctionKeyBar.axaml.cs
@@ -0,0 +1,17 @@
+using Avalonia.Controls;
+
+namespace AvaloniaFunctionKeyTemplate.Shell;
+
+/// <summary>
+/// Shell下部にF1からF12までの操作ガイドを表示する。
+/// </summary>
+public partial class FunctionKeyBar : UserControl
+{
+    /// <summary>
+    /// ファンクションキーバーのXAMLを初期化する。
+    /// </summary>
+    public FunctionKeyBar()
+    {
+        InitializeComponent();
+    }
+}
